a47d35d3c9230510661b41202751f69aa9f5b831
hamen <noeln4363@gmail.com>
2025-10-16 19:16:37 -0400

    Create how-to-use.js for README icon instructions
    Add usage instructions for icons in README.md

---
+++ how-to-use.js
@@ -0,0 +1,7 @@
+# Wondered how to use The Icons In The README.md 
+<h1> well heres how! <h1>
+
+  ```html
+  [!My Skills!](https://skillicons.dev/icons?i=html,cpp,js,c,ruby,css,php,typescript,python,r,go,flask,rust,unrealengine,bootstrap)](#)
+
+  <h1> just go ahead and paste that into your gitub profile reamde! <h1>
